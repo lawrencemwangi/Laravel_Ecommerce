4c4bded3f50c6bb950858be1087eafb4d2ebcc9f
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-02-06 23:52:59 +0300

    Ad the functionality for the routes and the login  functionlity with error handling ad displaying them to the user

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
 
-        if(!Auth::attemptgit($request->only('email', 'password'))) {
+        if(!Auth::attempt($request->only('email', 'password'))) {
             return response()->json(['message' => 'Invalid credentials.'], 401);
         }
 
